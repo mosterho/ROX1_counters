c10146405c7fc4f718399c9428e6ee91f6c736d4
Unknown <mosterho@ramapo.edu>
2020-05-18 11:52:04 -0400

    Initial commit

---
+++ ROX1_IMAP_access.py
@@ -0,0 +1,59 @@
+###
+## open the "dispatches" mailbox, accumulate, print and count
+## the fire and EMS incident numbers and their respective totals
+##
+
+import sys
+import imaplib, ssl, hashlib, json
+from cryptography.fernet import Fernet
+
+class cls_CAD_emails():
+    def __init__(self):
+        ### open file and retrieve email info
+        self.wrk_login, self.wrk_hash, self.wrk_pwd, self.wrk_salt = '', '', '', ''
+        ## read the json file to get values
+        self.fct_read_json()
+        ### initial test for SSL to server comes back with error for TLS
+        mailbox_context = ssl.create_default_context()
+        self.CADEmails = imaplib.IMAP4_SSL(host=self.wrk_host, port=993)
+        #self.CADEmails.starttls(ssl_context=mailbox_context)   # error message that TLS is not supported
+
+        self.CADEmails.login(self.wrk_login, self.wrk_pwd)
+        self.overall_firecount = 0
+        self.overall_emscount = 0
+        self.incident_list = []
+        # create empty passlib object
+
+    def fct_read_json(self):
+        wrk_jsonfile = open('../Additional/data.json')
+        wrk_json = json.load(wrk_jsonfile)
+        wrk_jsonfile.close()
+        self.wrk_publickey = wrk_json["publickey"]
+        self.wrk_salt = wrk_json["salt"]
+        self.wrk_host = wrk_json["host"]
+        self.wrk_login = wrk_json["login"]
+        ###
+        ### decrypt using cryptography/Fernet
+        # encode text to bytes
+        tmp_key = self.wrk_publickey.encode()
+        # create cypher object
+        cipher_suite = Fernet(tmp_key)
+        #
+        tmp_pwd = wrk_json["pwd"]
+        tmp_password = tmp_pwd.encode()
+        # decrypt password and set the class work variable
+        tmp_decrypted_password = cipher_suite.decrypt(tmp_password)
+        self.wrk_pwd = tmp_decrypted_password.decode()
+        #print("pwd:", (self.wrk_pwd))
+
+
+    def fct_cleanup(self):
+        self.CADEmails.logout()
+
+#############################################################
+### Begin mainline
+
+if (__name__ == "__main__"):
+
+    wrk_class = cls_CAD_emails()
+    wrk_class.fct_cleanup()
---
+++ ROX1_firecounter_IMAP.py
@@ -0,0 +1,94 @@
+###
+## open the "dispatches" mailbox, accumulate, print and count
+## the fire and EMS incident numbers and their respective totals
+##
+
+import sys, ROX1_IMAP_access
+from datetime import datetime, date, time
+from operator import itemgetter
+
+class cls_container():
+    def __init__(self, arg_mailbox_class):
+        self.mailbox_class = arg_mailbox_class
+        self.overall_firecount = 0
+        self.overall_emscount = 0
+        self.incident_list = []
+
+    def fct_read_email(self, arg_mailbox_class, arg_mailboxfolder):
+        arg_mailbox_class.CADEmails.select(mailbox=arg_mailboxfolder, readonly=True)
+        typ, data = arg_mailbox_class.CADEmails.search(None, '(BODY "3691") (SINCE 1-JAN-2020)')
+        for num in data[0].split():
+            typ2, data2 = arg_mailbox_class.CADEmails.fetch(num, "(BODY.PEEK[TEXT])")
+            ## For whatever reason, sometimes there is b')' in data2[0][1]
+            ## Use TRY: to avoid errors with  .decode
+            try:
+                decoded_bodytext = data2[0][1].decode()
+                split_bodytext = decoded_bodytext.splitlines()
+            except:
+                split_bodytext = ''
+            ## go through individual lines of email body, find incident number, etc.
+            tmp_startdate_flag, tmp_startdate_flag2 = False, False
+            for x in split_bodytext:
+                x_split = x.split()
+                if(x[:13] == 'Event Number:'):  #F201140011
+                    this_incident_nbr = x[14:24]
+                    tmp_return = self.fct_event_number(this_incident_nbr)
+                elif(x[:45] == 'Start Dt     Time       Situation/Description'):  ## Set flag to read next line, this contains the desired date_tuple
+                    tmp_startdate_flag = True
+                elif(tmp_startdate_flag and x[2:3] == '/' and x[5:6] == '/'): # 04/24/20 16:23:37  FND: 252   SMELL/ODOR/SOUND OF GAS LEAK INSIDE BUILD
+                    #tmp_startdate_flag2 = True
+                    inc_date = x_split[0]
+                    inc_time = x_split[1]
+                    tmp_special = x.split(maxsplit=4)  # special split to get full incident description
+                    inc_description = tmp_special[4]
+                    self.incident_list.append((this_incident_nbr, inc_date, inc_time, inc_description))
+                    break
+        arg_mailbox_class.CADEmails.close()
+
+    def fct_event_number(self, arg_incident_nbr):
+        tmp_incident_nbr = arg_incident_nbr
+        rtn_flag = ''
+        if(tmp_incident_nbr not in self.incident_list):
+            if(tmp_incident_nbr[:1] == 'F'):
+                self.overall_firecount += 1
+                rtn_flag = 'F'
+            if(tmp_incident_nbr[:1] == 'E'):
+                self.overall_emscount += 1
+                rtn_flag = 'E'
+        return rtn_flag
+
+    def fct_sortandprint(self):
+        print_ctr = 0
+        tmp_incident_list = sorted(self.incident_list, key=itemgetter(1,2))  #sort by date and time
+        for inc_data in tmp_incident_list:
+            print(inc_data)
+
+    def fct_finish(self, arg_mailbox_class):
+        #arg_mailbox_class.CADEmails.logout()
+        try:
+            arg_mailbox_class.fct_cleanup()
+        except:
+            print('Issue logging out of mailbox')
+        print('Overall fire_counter:', self.overall_firecount)
+        print('Overall  ems_counter:', self.overall_emscount)
+
+#############################################################
+### Begin mainline
+
+if (__name__ == "__main__"):
+
+    ## create a class object that connected to the mail server only
+    wrk_class = ROX1_IMAP_access.cls_CAD_emails()
+
+    ## create a second class that contains the mail server class from above
+    ## This will contain the functions, etc. to read the emails from the mail server class
+    wrk_container = cls_container(wrk_class)
+
+    # Read a nd process the emails in the INBOX
+    wrk_container.fct_read_email(wrk_class, 'INBOX')
+
+    # Sort and print the detailed results from reading the inbox
+    wrk_container.fct_sortandprint()
+
+    # print summary, perform cleanup on mail server
+    wrk_container.fct_finish(wrk_class)
