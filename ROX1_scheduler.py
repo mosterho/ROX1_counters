1c7b7c27bd2760875486f8cd1befc95f4abd4ad1
Unknown <mosterho@ramapo.edu>
2020-06-09 10:13:45 -0400

    Quick change

--- ROX1_scheduler.py
+++ ROX1_scheduler.py
@@ -12,6 +12,5 @@ while 1==1:
     print("\r{:60s}\r".format('Update running...'), end='', flush=True)
     subprocess.run(['python3', 'ROX1_update_ROX1db.py'])
     for i in range(wrk_time, 0, -60):
-        #print('\rJob sleeping... seconds remaining: ' + "{:4d}".format(i) + '\r', end='', flush=True)
         print("\r{:60s}\r".format('Job sleeping... time remaining: ' + "{:2d}".format(int(i/60)) + " minutes"), end='', flush=True)
         time.sleep(60)
